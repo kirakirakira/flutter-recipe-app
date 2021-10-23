491e0b8559e8e0f593fe43c9f6d1b791934ba8c4
Kira Hartlage <kira.hartlage@gmail.com>
2021-10-23 15:42:28 -0400

    add ingredient list

--- lib/recipe.dart
+++ lib/recipe.dart
@@ -2,38 +2,140 @@ class Recipe {
   String label;
   String description;
   String imageUrl;
+  int servings;
 
   Recipe(
     this.label,
     this.description,
     this.imageUrl,
+    this.servings,
+    this.ingredients,
   );
+
+  List<Ingredient> ingredients;
 
   static List<Recipe> samples = [
     Recipe(
       'Spaghetti and Beanballs',
       'I like to eat this with garlic bread.',
       'assets/2126711929_ef763de2b3_w.jpg',
+      4,
+      [
+        Ingredient(
+          1,
+          'box',
+          'Spaghetti',
+        ),
+        Ingredient(
+          4,
+          '',
+          'Frozen Beanballs',
+        ),
+        Ingredient(
+          0.5,
+          'jar',
+          'Rao\'s Marinara Sauce',
+        ),
+      ],
     ),
     Recipe(
       'Grilled Cheese',
       'Yummmy',
       'assets/3187380632_5056654a19_b.jpg',
+      1,
+      [
+        Ingredient(
+          2,
+          'slices',
+          'Cheese',
+        ),
+        Ingredient(
+          2,
+          'slices',
+          'Bread',
+        ),
+      ],
     ),
     Recipe(
       'Chocolate Chip Cookies',
       'get me some milk',
       'assets/15992102771_b92f4cc00a_b.jpg',
+      24,
+      [
+        Ingredient(
+          4,
+          'cups',
+          'flour',
+        ),
+        Ingredient(
+          2,
+          'cups',
+          'sugar',
+        ),
+        Ingredient(
+          0.5,
+          'cups',
+          'chocolate chips',
+        ),
+      ],
     ),
     Recipe(
       'Taco Salad',
       'where is the hot sauce?',
       'assets/8533381643_a31a99e8a6_c.jpg',
+      1,
+      [
+        Ingredient(
+          4,
+          'oz',
+          'nachos',
+        ),
+        Ingredient(
+          3,
+          'oz',
+          'crushed walnuts',
+        ),
+        Ingredient(
+          0.5,
+          'cup',
+          'vegan cheese',
+        ),
+        Ingredient(
+          0.25,
+          'cup',
+          'chopped tomatoes',
+        ),
+      ],
     ),
-    Recipe(
-      'Hawaiian Pizza',
-      'I love pineapple',
-      'assets/15452035777_294cefced5_c.jpg',
-    ),
+    Recipe('Hawaiian Pizza', 'I love pineapple',
+        'assets/15452035777_294cefced5_c.jpg', 4, [
+      Ingredient(
+        1,
+        'item',
+        'pizza',
+      ),
+      Ingredient(
+        1,
+        'cup',
+        'pineapple',
+      ),
+      Ingredient(
+        8,
+        'oz',
+        'vegan ham',
+      ),
+    ]),
   ];
+}
+
+class Ingredient {
+  double quantity;
+  String measure;
+  String name;
+
+  Ingredient(
+    this.quantity,
+    this.measure,
+    this.name,
+  );
 }
--- lib/recipe_detail.dart
+++ lib/recipe_detail.dart
@@ -25,35 +25,43 @@ class _RecipeDetailState extends State<RecipeDetail> {
           title: Text(widget.recipe.label),
         ),
         body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 300,
-                width: double.infinity,
-                child: Image(
-                  image: AssetImage(widget.recipe.imageUrl),
-                ),
-              ),
-
-              // spacer
-              const SizedBox(
-                height: 4,
-              ),
-
-              Text(
-                widget.recipe.label,
-                style: const TextStyle(fontSize: 18),
-              ),
-
-              Text(
-                widget.recipe.description,
-                style: const TextStyle(fontSize: 16),
-              ),
-              // todo: add expanded
-
-              // todo: add slider() here
-            ],
+            child: Column(children: <Widget>[
+          SizedBox(
+            height: 300,
+            width: double.infinity,
+            child: Image(
+              image: AssetImage(widget.recipe.imageUrl),
+            ),
           ),
-        ));
+
+          // spacer
+          const SizedBox(
+            height: 4,
+          ),
+
+          Text(
+            widget.recipe.label,
+            style: const TextStyle(fontSize: 18),
+          ),
+
+          Text(
+            widget.recipe.description,
+            style: const TextStyle(fontSize: 16),
+          ),
+
+          Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.all(7.0),
+              itemCount: widget.recipe.ingredients.length,
+              itemBuilder: (BuildContext context, int index) {
+                final ingredient = widget.recipe.ingredients[index];
+                // add ingredient quantity
+                return Text(
+                    '${ingredient.quantity} ${ingredient.measure} ${ingredient.name}');
+              },
+            ),
+          )
+        ])));
   }
 }
+              // todo: add slider() here
